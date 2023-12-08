7b4e9b2614ca4771efd0a87e86b5e492fcfb3ec6
chp320 <chp320@naver.com>
2023-12-08 23:49:16 +0900

    gitignore 파일 등록

--- messages/src/main/java/app/messages/AppConfig.java
+++ messages/src/main/java/app/messages/AppConfig.java
@@ -1,13 +1,15 @@
 package app.messages;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;    // '23.12.08, @Bean 어노테이션 붙은 메서드 주석 처리함으로써 import 도 주석 처리
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("app.messages")
+@ComponentScan("app.messages")    // 입력된 기본 패키지(여기서는 "app.messages")부터 스캔
 public class AppConfig {
-  
+
+  // '23.12.08, MessageService, MessageRepository 클래스에 @Component 어노테이션 추가함으로써 '빈 선언'하여 불필요한 하기 메서드는 주석 처리함
+  /*
   @Bean
   public MessageRepository messageRepository() {
     return new MessageRepository();
@@ -17,4 +19,5 @@ public class AppConfig {
   MessageService messageService() {
     return new MessageService(messageRepository());
   }
+  */
 }
