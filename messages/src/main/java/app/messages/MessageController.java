60c87f0e3cdc2b7b77f622b11f4d9f4897664e8e
chp320 <chp320@naver.com>
2023-12-09 01:01:17 +0900

    웹 애플리케이션 테스트

--- messages/src/main/java/app/messages/MessageController.java
+++ messages/src/main/java/app/messages/MessageController.java
@@ -3,6 +3,7 @@ package app.messages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * HTTP 요청을 받는 컨트롤러 기능 구현
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/messages")
 public class MessageController {
   @GetMapping("/welcome")
+  @ResponseBody
   public String welcome() {
     return "Hello, Welcome to Spring Boot!";
   }
