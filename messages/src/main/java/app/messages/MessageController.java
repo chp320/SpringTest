8fb4e1512edb3b9c8545430f0809a5dc0f0e785f
chp320 <chp320@naver.com>
2023-12-10 01:17:59 +0900

    불필요 파일 제거

--- messages/src/main/java/app/messages/MessageController.java
+++ messages/src/main/java/app/messages/MessageController.java
@@ -1,7 +1,5 @@
 package app.messages;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
