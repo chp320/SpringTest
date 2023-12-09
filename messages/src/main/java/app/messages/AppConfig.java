a65a8a9dbdc7aa4521425a0e060dd8c0078c7cf7
chp320 <chp320@naver.com>
2023-12-09 22:13:25 +0900

    filter 관련 설정 추가

--- messages/src/main/java/app/messages/AppConfig.java
+++ messages/src/main/java/app/messages/AppConfig.java
@@ -1,5 +1,9 @@
 package app.messages;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Bean;    // '23.12.08, @Bean 어노테이션 붙은 메서드 주석 처리함으로써 import 도 주석 처리
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,4 +24,21 @@ public class AppConfig {
     return new MessageService(messageRepository());
   }
   */
+
+  // '23.12.09, 필터 등록 (FilterRegistrationBean 생성)
+  /*
+   * 필터 처리하기 위해 생성한 AuditingFilter 를 스프링 부트에 등록하기 위한 2가지 방법 존재
+   * 방법1) web.xml 파일에 <filter>, <filter-mapping> 추가
+   * 방법2) FilterRegistrationBean 만들어 설정 클래스인 AppConfig에 등록
+   */
+  @Bean
+  public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean() {
+    FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<AuditingFilter>();
+    AuditingFilter filter = new AuditingFilter();
+    registration.setFilter(filter);
+    registration.setOrder(Integer.MAX_VALUE);   // ???
+    registration.setUrlPatterns(Arrays.asList("/messages/*"));    // /messages/ 로 시작하는 요청만 처리함
+
+    return registration;
+  }
 }
---
+++ messages/src/main/java/app/messages/AuditingFilter.java
@@ -0,0 +1,29 @@
+package app.messages;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * 서블릿에 도착하기 전, HTTP 요청에 대한 필터링 작업 수행을 위한 클래스(필터)
+ */
+public class AuditingFilter extends GenericFilterBean{
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+        long start = new Date().getTime();
+        chain.doFilter(req, res);
+
+        long elapsed = new Date().getTime() - start;
+        HttpServletRequest request = (HttpServletRequest) req;
+        logger.debug("Request[uri=" + request.getRequestURI() + ", method=" + request.getMethod() + "] completed in " + elapsed + " ms" );
+  }
+}
